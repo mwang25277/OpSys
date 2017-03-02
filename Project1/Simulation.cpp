ea6bb7c8700fcf0e7791b1bcb331e39b6883ae44
mwang25277 <wangm13@rpi.edu>
2017-03-01 21:30:50 -0500

    basically finished fcfs, just have to fix the timing

--- Project1/Simulation.cpp
+++ Project1/Simulation.cpp
@@ -21,14 +21,14 @@ Simulation::Simulation(std::vector<Process>& processs, std::string alg_) {
 		processes[p.getID()] = p;
 	}
 
-	//iterate through processes
-	std::map<std::string, Process>::iterator itr = processes.begin();
-	while(itr != processes.end()) {
-		Process p = itr->second;
-		//if a process hasn't completed it's CPU bursts, return false
-		std::cout << p.getID() << "|" << p.getBurstTime() << "|" << p.getNumBursts() << std::endl;
-		itr++;
-	}
+	// //iterate through processes
+	// std::map<std::string, Process>::iterator itr = processes.begin();
+	// while(itr != processes.end()) {
+	// 	Process p = itr->second;
+	// 	//if a process hasn't completed it's CPU bursts, return false
+	// 	std::cout << p.getID() << "|" << p.getBurstTime() << "|" << p.getNumBursts() << std::endl;
+	// 	itr++;
+	// }
 
 }
 
@@ -90,11 +90,31 @@ void Simulation::checkArrivals(int i) {
 
 void Simulation::runSimFCFS() {
 	
-	std::cout << "Time 0ms: Simulator started for FCFS [Q <empty>]" << std::endl;
+	std::cout << "time 0ms: Simulator started for FCFS [Q <empty>]" << std::endl;
 	while(!isDone()) {
 		checkArrivals(time);
 		//std::cout << cpu.getState() << std::endl;
 		//if the cpu is not being used
+
+				//iterate through all processes and decrement IO times for the processes that are blocked
+		std::map<std::string, Process>::iterator itr = processes.begin();
+		while(itr != processes.end()) {
+			Process p = itr->second;
+			if(p.getState() == "blocked") {
+				p.setIOTime(p.getIOTime() - 1);
+				if(p.getIOTime() == 0) {
+					p.setState("ready");
+					p.setIOTime(p.getInitialIOTime());
+					readyQueue.push_back(p);
+					std::cout << "time " << time << "ms: Process " << p.getID() << " completed I/O; added to ready queue " << outputQueue();
+				}
+				itr->second = p;
+			}
+
+			itr++;
+		}
+
+
 		if(cpu.getState() == "idle") {
 			if(!readyQueue.empty()) {
 				//start a process
@@ -105,43 +125,55 @@ void Simulation::runSimFCFS() {
 				cpu.setArrivingTime(t_cs / 2);
 			}
 		}
+
+		else if(cpu.getState() == "arriving") {
+			cpu.setArrivingTime(cpu.getArrivingTime() - 1);
+			if(cpu.getArrivingTime() == 0) {
+				cpu.setState("busy");
+				std::cout << "time " << time << "ms: Process " << cpu.getCurrProcess().getID() << " started using the CPU " << outputQueue();
+				Process p = cpu.getCurrProcess();
+				p.setState("running");
+				processes[p.getID()] = p;
+			}
+		}
+
 		else if(cpu.getState() == "busy") {
 			Process curr = cpu.getCurrProcess();
 			curr.setBurstTime(curr.getBurstTime() - 1);
 			if(curr.getBurstTime() == 0) {
 				curr.setNumBursts(curr.getNumBursts() - 1);
 				if(curr.getNumBursts() == 0) {
-					std::cout << "Process " << curr.getID() << " terminated " << outputQueue();
+					std::cout << "time " << time << "ms: Process " << curr.getID() << " terminated " << outputQueue();
+					curr.setState("done");
 				}
 				else {
-					std::cout << "Process " << curr.getID() << " completed a CPU burst; ";
+					std::cout << "time " << time << "ms: Process " << curr.getID() << " completed a CPU burst; ";
 					std::cout << curr.getNumBursts() << " to go " << outputQueue();
-					std::cout << "Process " << curr.getID() << "switching out of CPU; will block on I/O until time " << time + curr.getIOTime();
+					std::cout << "time " << time << "ms: Process " << curr.getID() << " switching out of CPU; will block on I/O until time " << time + curr.getIOTime() + t_cs / 2;
 					std::cout << " " << outputQueue();
 					curr.setBurstTime(curr.getInitialBurstTime()); //reset burst time
 				}
-
-				curr.setState("blocked");
 				cpu.setState("leaving");
 				cpu.setLeavingTime(t_cs / 2);
 				//update the map????? maybe?? probably??
 				
 			}
+			//basically saving changes to the process
 			cpu.setCurrProcess(curr);
+			processes[curr.getID()] = curr;
 
 		}
-		else if(cpu.getState() == "arriving") {
-			cpu.setArrivingTime(cpu.getArrivingTime() - 1);
-			if(cpu.getArrivingTime() == 0) {
-				cpu.setState("busy");
-				std::cout << "Process " << cpu.getCurrProcess().getID() << " started using the CPU " << outputQueue();
-				cpu.getCurrProcess().setState("running");
-			}
-		}
+		
 		else if(cpu.getState() == "leaving") {
 			cpu.setLeavingTime(cpu.getLeavingTime() - 1);
 			if(cpu.getLeavingTime() == 0) {
 				cpu.setState("idle");
+				Process p = cpu.getCurrProcess();
+				if(p.getNumBursts() != 0) {
+					p.setState("blocked");
+					processes[p.getID()] = p;
+				}
+				//cpu.setCurrProcess(NULL);
 			}
 		}
 
